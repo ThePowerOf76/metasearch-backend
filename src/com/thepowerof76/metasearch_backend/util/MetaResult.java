b9af52049af900445a528750f3d85ee2fdb91ca0
TheVintageTyrolean <thevintagetyrolean@gmail.com>
2023-05-30 20:36:07 +0200

    Recovered MetaResult and Pair

---
+++ src/com/thepowerof76/metasearch_backend/util/MetaResult.java
@@ -0,0 +1,21 @@
+package com.thepowerof76.metasearch_backend.util;
+
+import java.util.Arrays;
+public class MetaResult extends Result {
+    private final boolean[] resultSrc = new boolean[6];
+
+    public MetaResult(String t, String h, String d, int id) {
+        super(t, h, d, id);
+        Arrays.fill(resultSrc, false);
+    }
+    public boolean[] getSources() {
+        return resultSrc;
+    }
+    public void foundInSource(int sourceID) {
+        if(sourceID > 0 && sourceID < 7) {
+            resultSrc[sourceID-1] = true;
+        } else {
+            System.out.println("Wrong engine ID");
+        }
+    }
+}
---
+++ src/com/thepowerof76/metasearch_backend/util/Pair.java
@@ -0,0 +1,10 @@
+package com.thepowerof76.metasearch_backend.util;
+
+public class Pair<A, B> {
+    public A first;
+    public B second;
+    public Pair(A one, B two) {
+        first = one;
+        second = two;
+    }
+}
